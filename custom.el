156ad86615e82aa230451dfb2d1c29efb27778fb
Suraj Thanugundla <Suraj Thanugundla>
2017-04-27 21:00:15 +0530

    Refactoring

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(confirm-nonexistent-file-or-buffer nil))
+ '(package-selected-packages
+   (quote
+    (multiple-cursors use-package swiper solarized-theme smartparens rainbow-delimiters py-autopep8 neotree magit helm-swoop helm-projectile flycheck elpy doom-themes dashboard better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
