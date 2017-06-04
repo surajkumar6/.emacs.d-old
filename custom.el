fb753bcbd6a871de6cab1b1d76205a65de2301ef
Suraj Thanugundla <suraj.kumar.reddy@gmail.com>
2017-06-04 18:13:49 +0530

    Theme tweaks

--- custom.el
+++ custom.el
@@ -3,12 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-fontify-quote-and-verse-blocks t)
  '(package-selected-packages
    (quote
-    (multiple-cursors use-package swiper solarized-theme smartparens rainbow-delimiters py-autopep8 neotree magit helm-swoop helm-projectile flycheck elpy doom-themes dashboard better-defaults))))
+    (multiple-cursors use-package swiper solarized-theme smartparens rainbow-delimiters py-autopep8 neotree magit helm-swoop helm-projectile flycheck elpy doom-themes dashboard better-defaults)))
+ '(projectile-keymap-prefix "C-c p"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:background "#FFFFFF" :foreground "#000088"))))
+ '(org-block-background ((t (:background "#FFFFFF"))))
+ '(org-level-1 ((t (:background "#FFFFFF" :foreground "#3C3C3C" :overline nil :weight bold :height 1.0))))
+ '(org-level-2 ((t (:background "#FFFFFF" :foreground "#0077CC" :overline nil :weight bold :height 1.0))))
+ '(org-level-3 ((t (:background "#FFFFFF" :foreground "#005522" :weight bold :height 1.0))))
+ '(org-level-6 ((t (:foreground "#123555" :slant italic :weight bold :height 1.0)))))
